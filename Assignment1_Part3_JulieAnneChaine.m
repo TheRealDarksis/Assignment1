7fe89ffa3437d3cc1615a0cc77cdad29283de834
Julie-Anne Chaine <violinmaster9010@gmail.com>
2022-03-19 16:57:10 -0400

    Resubmission Commit

--- Assignment1_Part3_JulieAnneChaine.m
+++ Assignment1_Part3_JulieAnneChaine.m
@@ -22,7 +22,6 @@ y = zeros(elecpop,1);               % Electron x positions
 oldx = zeros(elecpop,1);            % Previous electron x position
 oldy = zeros(elecpop,1);            % Previous electron y position
 Vth = sqrt(2*k*T/mn);               % Thermal velocity in m/s
-n = zeros(elecpop,1);               % Velocity distribution 
 mfp = Vth*tau_mn;                   % Mean free path in m
 samplepop = 10;                     % # of particles to plot
 samp = randi(elecpop,samplepop,1);  % Random particles to observe
@@ -73,34 +72,44 @@ circle = [circx,circy];
         x(x>W) = x(x>W) - W;    % All points passed the right get pushed to the other side
         vy(y>L) = -vy(y>L);     % Particle Y direction gets flipped if it hits the top
         vy(y<0) = -vy(y<0);     % Particle Y direction gets flipped
-        figure(2)        
-        circle2(0,0.5e-7,0.2e-7);   % Plotting circle
-        for m = 1:samplepop
-            for a = 1:2
-                rectangle('Position',[Boxes{a}.X(1), Boxes{a}.Y(1), Boxes{a}.X(2), Boxes{a}.Y(2)])
-                axis([0 W 0 L])
-            end            
-            plot([oldx(samp(m)), x(samp(m))],[oldy(samp(m)),y(samp(m))],'SeriesIndex',m)
-            title('Sample Particles Trajectories')
-            xlabel('W (m) or x position')
-            ylabel('L (m) or y position')
-            axis([0 200e-9 0 100e-9])
-            hold on 
-        end
-            V = mean( sqrt( vx.^2+vy.^2 ) );
-            NewT = V.^2*mn/(2*k);  
-            figure(3)% Plotting T
-            plot(j,NewT,'o')
-            hold on
-            title('Temperature at random points')
-            xlabel('random occurence')
-            ylabel('Temperature (K)')
+%         figure(2)        
+%         circle2(0,0.5e-7,0.2e-7);   % Plotting circle
+%         for m = 1:samplepop
+%             for a = 1:2
+%                 rectangle('Position',[Boxes{a}.X(1), Boxes{a}.Y(1), Boxes{a}.X(2), Boxes{a}.Y(2)])
+%                 axis([0 W 0 L])
+%             end            
+%             plot([oldx(samp(m)), x(samp(m))],[oldy(samp(m)),y(samp(m))],'SeriesIndex',m)
+%             title('Sample Particles Trajectories')
+%             xlabel('W (m) or x position')
+%             ylabel('L (m) or y position')
+%             axis([0 200e-9 0 100e-9])
+%             hold on 
+%         end
+            V = sqrt( vx.^2+vy.^2 ); 
         pause(0.01)
     end
 
-Plotting electron density maps
+% % Plotting electron density maps
+% figure
+% hist3([x(:), y(:)],'CDataMode','auto')
+% xlabel('W (m) or x position')
+% ylabel('L (m) or y position')
+% title('Electron density map')
+
+%temp map!!!!
+nx = 20; ny = 30;
+BinX = ceil( x*nx/W );
+BinY = ceil( y*ny/L );
+for i = 1:nx
+    for j = 1:ny
+        match = BinX == i & BinY == j;
+        Tbox(i,j) = sum( V(match).^2/(2*k) );
+    end
+end
 figure
-hist3([x(:), y(:)],'CDataMode','auto')
-xlabel('W (m) or x position')
-ylabel('L (m) or y position')
-title('Electron density map, rotate to get top view')
+surf(Tbox)
+title('Temperature Density')
+xlabel('Width (m)')
+ylabel('Length (m)')
+zlabel('Temperature (K)')
